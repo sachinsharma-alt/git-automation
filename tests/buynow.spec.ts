8d99ffdb5d13c45923febf732efe1ef6ab77e762
sachin <sachin.sharma@beowned.com>
2026-09-21 18:08:31 +0530

    updated

--- tests/buynow.spec.ts
+++ tests/buynow.spec.ts
@@ -6,9 +6,9 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'ruby appartment ruby' }).click();
   await page.getByRole('button', { name: 'Buy now' }).first().click();
   await page.getByRole('textbox', { name: 'Email Id or mobile number' }).click();
-  await page.getByRole('textbox', { name: 'Email Id or mobile number' }).fill('ashwin@beowned.com');
+  await page.getByRole('textbox', { name: 'Email Id or mobile number' }).fill('shedgeadi31@gmail.com');
   await page.getByRole('textbox', { name: 'Password Show password' }).click();
-  await page.getByRole('textbox', { name: 'Password Show password' }).fill('Ashwin@2oo7');
+  await page.getByRole('textbox', { name: 'Password Show password' }).fill('Adi@#3112');
   await page.getByRole('textbox', { name: 'Password Show password' }).press('Enter');
   await page.getByRole('checkbox', { name: 'I hereby confirm to receive \'' }).check();
   await page.getByRole('checkbox', { name: 'I agree to the Terms and' }).check();
